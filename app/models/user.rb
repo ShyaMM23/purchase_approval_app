cacdc4f0f22feb20caa629b2d1f6e21ecb401637
ShyaMM23 <10727deepak@gmail.com>
2026-06-18 20:16:11 +0530

    Day 12 exercise: Add user status enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
     approver: 1,
     admin: 2
   }
+  enum :status, {
+    active: 0,
+    inactive: 1,
+    suspended: 2
+  }
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
+  validates :status, presence: true
 end
